88c68d4a279a007ddfb7db8e45b21f1f5bb85d63
Jessica <Jagenua@GMAIL.com>
2016-07-12 21:28:39 -0500

    minicapstone redirect and flash updates

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,45 +1,50 @@
 class DogsController < ApplicationController
+  def index
+    @dog = Dog.all
+  end  
 
-	def index
-		@dogs = Dog.all
-	end	
+  def show
+    @dog = Dog.find_by(id: params['id'])
+    render 'show.html.erb'
+  end
+  
+  def new
+    render 'new.html.erb'
+  end
 
-	def show
-		@dogs = Dog.find_by(id: params['id'])
-		render 'show.html.erb'
-	end
-	
-	def new
-		render 'new.html.erb'
-	end
+  def create
+    @dog = Dog.new( 
+      breed: params['breed'],
+      sex: params['sex'],
+      age: params['age'],
+      img: params['img'],
+      price: params['price']
+    )
+    @dog.save
+    flash[:success] = "This dog has been added to our Adoption center"
+    redirect_to "/dog_store/#{@dog.id}"
+  end
 
-	def create
-		@dogs = Dog.new(	
-			breed: params['breed'],
-			sex: params['sex'],
-			age: params['age'],
-			img: params['img'],
-			price: params['price']
-			)
-		@dogs.save
-		render 'create.html.erb'
-	end
+  def edit
+    @dog = Dog.find_by(id: params['id'])
+    render 'edit.html.erb'
+  end
 
-	def edit
-	    @dogs = Dog.find_by(id: params['id'])
-    	render 'edit.html.erb'
-	end
+  def update
+    @dog = Dog.find_by(id: params['id'])
+    @dog.update(
+      breed: params['breed'],
+      sex: params['sex'],
+      age: params['age'],
+      img: params['img'],
+      price: params['price']
+    )
+    render 'update.html.erb'
+  end
 
-	def update
- 	   @dogs = Dog.find_by(id: params['id'])
- 	   @dogs.update(
-		    breed: params['breed'],
-			sex: params['sex'],
-			age: params['age'],
-			img: params['img'],
-			price: params['price']
-     )
-      render 'update.html.erb'
-    end
-
+  def destroy
+    @dog = Dog.find_by(id: params[:id])
+    @dog.delete
+    redirect_to '/dog_store'
+  end
 end
